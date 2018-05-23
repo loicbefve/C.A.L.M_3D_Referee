b9c044be70e5dd7131017cbf01b5f75fe3669646
loicbefve <befve.loic@gmail.com>
2018-05-23 15:03:30 +0200

    Ajout des assets du jeu pc

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody m_RigidBody;
+	private Animator m_Animator;
+
+    public float maxSpeed = 5.0f;
+    private bool facingRight = false;
+    private bool facingDown = true;
+
+    // Use this for initialization
+    void Start () {
+        m_RigidBody = GetComponent<Rigidbody>();
+		m_Animator = GetComponentInChildren<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        //Valeur entre -1 et 1 selon intentsité de frappe sur l'axe horizontal
+        float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+        //Fonction responsable du mouvement
+        Debug.Log("facing Down :" + v);
+        Debug.Log("facing Right :" + h);
+        MovePlayer(h, v);
+    }
+
+	void MovePlayer( float h, float v )
+    {
+        m_RigidBody.velocity = new Vector3(h * maxSpeed, 0, v * maxSpeed);
+
+        SetBool_H_V(h, v);
+
+    }
+
+    void SetBool_V(float v)
+    {
+        if (v > 0)
+        {
+            m_Animator.SetBool("GoUp", true);
+            m_Animator.SetBool("GoDown", false);
+        }
+        else if (v < 0)
+        {
+            m_Animator.SetBool("GoUp", false);
+            m_Animator.SetBool("GoDown", true);
+        }
+        else
+        {
+            m_Animator.SetBool("GoUp", false);
+            m_Animator.SetBool("GoDown", false);
+        }
+
+    }
+
+    void SetBool_H_V(float h, float v)
+    {
+        if (h > 0)
+        {
+            m_Animator.SetBool("GoLeft", false);
+            m_Animator.SetBool("GoRight", true);
+            SetBool_V(v);
+        }
+        else if (h < 0)
+        {
+            m_Animator.SetBool("GoLeft", true);
+            m_Animator.SetBool("GoRight", false);
+            SetBool_V(v);
+        }
+        else
+        {
+            m_Animator.SetBool("GoLeft", false);
+            m_Animator.SetBool("GoRight", false);
+            SetBool_V(v);
+        }
+    }
+
+    void FlipH()
+    {
+        facingRight = !facingRight;
+        
+        Vector3 s = transform.localScale;
+        s.x *= -1;
+        transform.localScale = s;
+    }
+
+    void FlipV()
+    {
+        facingDown = !facingDown;
+
+        Vector3 s = transform.localScale;
+        s.y *= -1;
+        transform.localScale = s;
+    }
+}
---
+++ Assets/Scripts/Starburst/SatelliteHandler.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarburstGaming;
+
+public class SatelliteHandler : MonoBehaviour {
+
+    PolledComponent battleComp;
+
+    void Awake()
+    {
+    #if UNITY_IOS || UNITY_ANDROID
+        EngineMobile.Start();
+    #endif
+
+        SatelliteLibrary.Init();
+
+        // Register Satellite and Component
+        Satellite.Register("FiL_BattleSat");
+        battleComp = new PolledComponent("FiL_Battle");
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        if (SatelliteLibrary.Running)
+        {
+            // Execute OnSessionFound when a session has been found
+            EngineExtensions.OnSessionJoined(SessionJoined);
+        }
+
+    }
+
+    void SessionJoined()
+    {
+        // This simple satellite does not have any particular action to execute on START,
+        // just signal that we are started.
+        battleComp.OnComponentStart(() => battleComp.ReportState(ComponentState.STARTED));
+
+        // Just Quit the application when the Component is stopped
+        battleComp.OnComponentStop(Application.Quit);
+    }
+
+    void OnApplicationQuit()
+    {
+        // If we are currently in a session, we can report that we are now stopped
+        if (Engine.InSession) battleComp.ReportState(ComponentState.STOPPED);
+
+        SatelliteLibrary.Close();
+
+    #if UNITY_IOS || UNITY_ANDROID
+        EngineMobile.Stop();
+    #endif
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
